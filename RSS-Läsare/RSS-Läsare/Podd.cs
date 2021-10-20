0170a459b7677eb121a4dfb3f3e0bbb687993a6e
ArvidNordqvist <62065432+ArvidNordqvist@users.noreply.github.com>
2021-10-20 14:13:55 +0200

    Changes
    3 Layering

---
+++ RSS-Läsare/RSS-Läsare/Podd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace RSS_Läsare
+{
+    [XmlInclude(typeof(Podd))]
+
+    public class Podd
+    {
+        public string Name { get; set; }
+        public int Avsnitt { get; set; }
+        public int Frekvens { get; set; }
+        public string Kategori { get; set; }
+        public Podd(string name, int avsnitt, int frekvens, string kategori)
+        {
+            Name = name;
+            Avsnitt = avsnitt;
+            Frekvens = frekvens;
+            Kategori = kategori;
+        }
+
+    }
+}
+
